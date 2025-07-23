6d51fd88672d9f0ebc8dd7f750ca01e447472914
riaztos <riaz@topofstacksoftware.dev>
2025-07-23 20:44:24 +0600

    initializing the todo app project

---
+++ src/test/java/com/riazulislam/todospringboot/TodoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riazulislam.todospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
