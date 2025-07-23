c7a11085527ab91d67ca52f77c96e97114eb5a8b
riaztos <riaz@topofstacksoftware.dev>
2025-07-23 23:39:50 +0600

    added post request controller and response entity

--- src/main/java/com/riazulislam/todospringboot/TodoController.java
+++ src/main/java/com/riazulislam/todospringboot/TodoController.java
@@ -1,12 +1,18 @@
 package com.riazulislam.todospringboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/api/v1/todo")
 public class TodoController {
 
     public static List<Todo> todoList;
@@ -18,8 +24,14 @@ public class TodoController {
         todoList.add(new Todo(2, "Todo 2", false, 1));
     }
 
-    @GetMapping("/")
-    public List<Todo> getAllTodos(){
-        return todoList;
+    @GetMapping
+    public ResponseEntity<List<Todo>> getAllTodos(){
+        return ResponseEntity.ok(todoList);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<Todo>> createNewTodo(@RequestBody Todo newTodo) {
+        todoList.add(newTodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoList);
     }
 }
